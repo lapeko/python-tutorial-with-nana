0cdb418aa37f9e49f45b68b3e2cb4f156a740d77
lapeko <lapeko88@gmail.com>
2023-11-05 23:34:38 +0100

    read cmd line. cast to int and calulate number of minutes

---
+++ main.py
@@ -0,0 +1,7 @@
+def calculate_hours(number_of_days):
+    print(number_of_days * 24 * 60)
+
+
+days_input = input("Enter number of days\n")
+number_of_days = int(days_input)
+calculate_hours(number_of_days)
